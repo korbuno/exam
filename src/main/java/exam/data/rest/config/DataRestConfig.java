c7d0da2d93e5eb68f5dc94f6d79e2ac0cbe57ba7
hyungooyun <aileen528@naver.com>
2019-04-22 18:25:44 +0900

    RestRepositoryController 적요 완료

--- src/main/java/exam/data/rest/config/DataRestConfig.java
+++ src/main/java/exam/data/rest/config/DataRestConfig.java
@@ -1,8 +1,10 @@
 package exam.data.rest.config;
 
+import exam.data.rest.securityTest.UserEntity;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
 
 @Configuration
 public class DataRestConfig implements RepositoryRestConfigurer{
@@ -13,5 +15,12 @@ public class DataRestConfig implements RepositoryRestConfigurer{
 //        TODO : 요청된 포트에 따라 설정을 바꿔갈수는 없을까
 //        config.setDefaultMediaType(MediaType.APPLICATION_JSON);
 //        config.useHalAsDefaultJsonMediaType(false);
+
+        // Data Repository 노출 메서드에 Http method 제한걸기
+        config.getExposureConfiguration()
+                .forDomainType(UserEntity.class)
+                    .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(HttpMethod.DELETE, HttpMethod.PATCH)))
+                    .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(HttpMethod.DELETE))
+                    .withAssociationExposure(((metdata, httpMethods) -> httpMethods.disable(HttpMethod.DELETE, HttpMethod.PATCH)));
     }
 }
